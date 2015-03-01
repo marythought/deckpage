c3fdb2519d54dabab2bcfc63b5032f1483b06954
Mary Dickson <mdickson@gmail.com>
2015-02-28 23:29:48 -0800

    updated page to play war w/computer

--- js/deck.js
+++ js/deck.js
@@ -47,9 +47,66 @@ $('#runButton').on('click', function() {
 });
 
 $('#dealButton').on('click', function() {
-  $("#runButton").css('display', 'none')
+  $('#runButton, #dealButton').addClass('hide')
+  $('#compareButton').removeClass('hide')
   var Deal = function() {
-    $( "li:odd" ).prependTo( "#computerDeck" );
+    $( "li:odd" )
+      .prependTo( "#computerDeck" )
+      .addClass('computerCard')
+      .removeClass('card');
   }
   Deal();
+});
+
+var WhoWins = function(card1,card2) {
+  //takes in 2 numbers or strings and returns the winner, or zero for war
+  if (card1[0] == card2[0]) {
+    return false;
+  } else if (card1[0] == "A") {
+    return card1;
+  } else if (card2[0] == "A") {
+    return card2;
+  } else if (card1[0] == "K") {
+    return card1;
+  } else if (card2[0] == "K") {
+    return card2;
+  } else if (card1[0] == "Q") {
+    return card1;
+  } else if (card2[0] == "Q") {
+    return card2;
+  } else if (card1[0] == "J") {
+    return card1;
+  } else if (card2[0] == "J") {
+    return card2;
+  } else if (card1[1] == "0") {
+    return card1;
+  } else if (card2[1] == "0") {
+    return card2;
+  } else if (card1 > card2) {
+    return card1;
+  } else {
+    return card2;
+  }
+}
+
+$('#compareButton').on('click', function() {
+  var Compare = function() {
+    var youplay = $(".card:first-child").text();
+    var computerplays = $(".computerCard:first-child").text();
+    var message = "You play " + String(youplay) + " Computer plays " + String(computerplays);
+    var winner = WhoWins(youplay, computerplays);
+    if (winner == youplay) {
+      $(".card:first-child, .computerCard:first-child").appendTo( "#deck" );
+      var result = "You win the round! Cards moved to your hand";
+    } else if (winner == computerplays) {
+      $(".card:first-child, .computerCard:first-child").appendTo( "#computerDeck" );
+      result = "Computer wins the round. Cards moved to computer's hand";
+    } else {
+      $(".card:first-child, .computerCard:first-child").remove();
+      result = "A tie! Nobody gets these cards..."
+    }
+  $("#playtext").text(message);
+  $("#resultstext").text(result);
+  }
+  Compare();
 });
